6d106592e38ad80ba066d104d3398e5ac8311f9f
Brian Wang <wangbrian26@gmail.com>
2022-12-14 16:36:14 -0800

    updated notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,24 @@ const notes = require("express").Router()
 const fs = require("fs")
 const { join } = require("path")
 
-notes.get("/", (req, res => 
+notes.get("/", (req, res => {
+  console.log(`${req.method} request received!`);
+  fs.readFile(join(__dirname, "..", "db", "db.json")).then((data) => {
+    res.json(JSON.parse(data))
+  });
+}))
+
+notes.post("/", (req, res => {
   console.log(`${req.method} request received!`)
-  fs.readFile(join(__dirname, "..", "db", "db.json")).then((data) => res.json(JSON.parse(data)))  
-))
+  fs.appendFile(req.body, join(__dirname, "..", "db", "db.json")).then((data) => {
+    res.
+  })
+}))
+
+notes.delete("/:id", (req, res => {
+  console.log(`${req.method} request received!`)
+  fs.readFile(join(__dirname, "..", "db", "db.json")).then((data, {id: req.parama.id}), (err) => {
+    if (err) throw err;
+    if data.
+  })
+}))
