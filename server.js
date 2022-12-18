b2d3b7b24df37a70de31be35bf9e02301e55fa93
Brian Wang <wangbrian26@gmail.com>
2022-12-18 10:36:05 -0800

    updated PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { join } = require("path");
 const api = require("./routes");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
